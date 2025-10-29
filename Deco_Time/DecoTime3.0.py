24bf992958b6d2ea3b805c00d2ac477e141482ea
VictorE121 <ramirez.victor.ep@gmail.com>
2025-10-29 03:20:13 -0600

    DecoTime 3 y 4 Listos

--- Deco_Time/DecoTime3.0.py
+++ Deco_Time/DecoTime3.0.py
@@ -1,32 +1,52 @@
 import time
+import sys
+sys.set_int_max_str_digits(0)
 
-Num1 = int(input("Enter a number: "))
+print(f"PRACTICE 003: DECORATOR TO MEASURE EXECUTION TIME")
+print(f"WARINING: For numbers bigger than 1000, If you are printing the output of products, this program may take a looong time.")
+print(f"WARINING: If also you print the output of sums, this program may take an extra looooong time.")
+Num1 = int(input(f"Enter a number: "))
 
 def Deco_Time(func):
     def wrapper(*args, **kwargs):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        print(f"Execution time: {end_time - start_time} seconds")
+        print(f"Execution time: {end_time - start_time} Seconds")
         return result
     return wrapper
 
+#1ST OPTION OF SUMA_NUM FUNCTION WITHOUT PRINTING INTERMEDIATE STEPS
+
+#@Deco_Time
+#def Suma_Num_0(Num1):
+#    return sum(range(1, Num1 + 1))
+
+#2ND OPTION OF FUNCION SUMA_NUM WITH PRINTING INTERMEDIATE STEPS
+
 @Deco_Time
-def Suma_Num(Num1):
-    for i in range(1, Num1 + 1):
-        print(f"Adding: {i}")
+def Suma_Num_0(Num1):
+    NumTemp = int(0)
+    for i in range(Num1):
+        #print(f"Adding: {NumTemp} + 1")
+        NumTemp += 1
+        #print(f"Result: {NumTemp}")
         #time.sleep(1)
     return Num1
 
+#FUNCTION PRODUCTO_NUM
+#Disabe printing intermediate steps for big numbers to avoid long execution times and clean output
+
 @Deco_Time
 def Producto_Num(Num1):
-    Prod_Num = 1
+    Prod_Num = int(1)
     for i in range(1, Num1 + 1):
-        print(f"Multiplying by: {i}")
+        #print(f"Multiplying: {Prod_Num} * {i}")
         Prod_Num *= i
-        print(f"Multiplying: {Prod_Num}")
+        #print(f"Result: {Prod_Num}")
         #time.sleep(1)
     return Prod_Num
 
-Suma_Num(Num1)
+
+Suma_Num_0(Num1)
 Producto_Num(Num1)
---
+++ Deco_Time/DecoTime4.0.py
@@ -0,0 +1,54 @@
+import time
+import sys
+sys.set_int_max_str_digits(0)
+
+print(f"PRACTICE 003: DECORADOR PARA MEDIR TIEMPO DE EJECUCIÓN")
+print(f"WARINING: Para numeros mayores a 1000, si imprimes el output de productos, este programa puede tardar muchooo tiempo.")
+print(f"WARINING: Si tambien imprimes el output de sumas, este programa puede tardar aun mas tiempo.")
+Num1 = int(input(f"Ingresa un Numero: "))
+
+def Deco_Time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Tiempo de Ejecucion: {end_time - start_time} Segundos")
+        return result
+    return wrapper
+
+
+#1RA OPCION DE LA FUNCION SUMA_NUM SIN IMPRESION DE PASOS INTERMEDIOS
+
+#@Deco_Time
+#def Suma_Num_0(Num1):
+#    return sum(range(1, Num1 + 1))
+
+
+#2DA OPCION DE LA FUNCION SUMA_NUM CON IMPRESION DE PASOS INTERMEDIOS
+
+@Deco_Time
+def Suma_Num_0(Num1):
+    NumTemp = int(0)
+    for i in range(Num1):
+        #print(f"Sumando: {NumTemp} + 1")
+        NumTemp += 1
+        #print(f"Resultado: {NumTemp}")
+        #time.sleep(1)
+    return Num1
+
+#FUNCTION PRODUCTO_NUM
+#Desabilita la impresión de pasos intermedios en números grandes para evitar largos tiempos de ejecución y una salida limpia
+
+@Deco_Time
+def Producto_Num(Num1):
+    Prod_Num = int(1)
+    for i in range(1, Num1 + 1):
+        #print(f"Multiplicando: {Prod_Num} * {i}")
+        Prod_Num *= i
+        #print(f"Resultado: {Prod_Num}")
+        #time.sleep(1)
+    return Prod_Num
+
+
+Suma_Num_0(Num1)
+Producto_Num(Num1)
