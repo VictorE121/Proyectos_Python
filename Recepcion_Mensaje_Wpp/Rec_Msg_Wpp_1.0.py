a4dfee57bb0cf39d5efff734af8dff8f78f4395c
VictorE121 <ramirez.victor.ep@gmail.com>
2025-10-30 14:39:36 -0600

    REC_MSG_WPP_1.0

---
+++ Recepcion_Mensaje_Wpp/Rec_Msg_Wpp_1.0.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+options = webdriver.ChromeOptions()
+options.add_argument(r"user-data-dir=C:\Users\tmaquinaria\Desktop\VIC\GIT\Proyectos_Python\Recepcion_Mensaje_Wpp\DatosUser")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension",False)
+driver = webdriver.Chrome(options=options)
+driver.maximize_window()
+driver.get('https://web.whatsapp.com')
+
+time.sleep(20)
